a5227f926c72c2186b139606ed97896e4fca1e2d
Isaque Streme Tavaiol <isaque.tavaiol@aluno.gaspar.sc.gov.br>
2025-11-02 11:36:31 +0000

    olha

--- src/components/dashboard/service-profit-chart.tsx
+++ src/components/dashboard/service-profit-chart.tsx
@@ -59,7 +59,7 @@ const ServiceProfitChart = () => {
           ) : (
             <BarChart 
               data={chartData}
-              margin={{ top: 20, right: 20, left: -10, bottom: 5 }}
+              margin={{ top: 20, right: 20, left: 10, bottom: 5 }}
               accessibilityLayer
             >
               <CartesianGrid vertical={false} />
@@ -71,7 +71,7 @@ const ServiceProfitChart = () => {
               />
               <YAxis 
                   tickFormatter={(value) => `R$${value/1000}k`}
-                  width={60}
+                  width={50}
               />
               <ChartTooltip
                 cursor={false}
