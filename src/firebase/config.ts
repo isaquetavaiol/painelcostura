10b9120b712be5a778a175a4f48aac271079211a
Isaque Streme Tavaiol <isaque.tavaiol@aluno.gaspar.sc.gov.br>
2025-11-02 11:04:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-499681828-8bfa2",
+  "appId": "1:735506151712:web:be673de50dcaa3e2a43f45",
+  "apiKey": "AIzaSyCYv4zAkmz-HltUm_G6s3amyq-wEFXH3dE",
+  "authDomain": "studio-499681828-8bfa2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "735506151712"
+};
