6ccf259582361f242b872ad43b555278080f3dff
Isaque Streme Tavaiol <isaque.tavaiol@aluno.gaspar.sc.gov.br>
2025-11-02 11:42:46 +0000

    e tao simples vc arrumar

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -23,7 +23,7 @@ const getTrophyColor = (rank: number) => {
 export default function LeaderboardPage() {
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <main className="flex-1 space-y-6 p-4 md:p-8 pt-6 mb-28">
+      <main className="flex-1 space-y-6 p-4 md:p-8 pt-6 mb-32">
         <h1 className="text-3xl font-bold">Leaderboard</h1>
          <Card>
           <CardHeader>
